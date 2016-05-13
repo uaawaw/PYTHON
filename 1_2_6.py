6f4ecc9d853d50551de544139c52e5c4da6dbe29
uaawaw <uaawaw@gmail.com>
2016-05-13 17:17:28 +0300

    something has changed

--- 1_2_6.py
+++ 1_2_6.py
@@ -1,14 +1,27 @@
 a = 7
 c = 1
 b = []
-
+z = 1
+x = [1]
 while c != a+1:
     if c != a+1:
         b.append(c)
         c = c+1
-        print (c)
+        #print (c)
     else:
         print (c)
     continue
 print (b)
+
+
+for i in b:
+    while z != i:
+        if z != i:
+            z = z+1
+            x.append(z)
+            print (x)
+        else:
+            print()
+    #print(x)
+    
         
