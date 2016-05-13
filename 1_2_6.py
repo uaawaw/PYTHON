f746ef6009e4a4574f912381a174c9287bf56b1d
uaawaw <uaawaw@gmail.com>
2016-05-13 14:19:51 +0300

    work n -> [1, 2, 3, ... n]

--- 1_2_6.py
+++ 1_2_6.py
@@ -1,21 +1,14 @@
-l = (1, -3, 5, -6, -10, 13, 5, 22, -15)
-b = 0
+a = 7
+c = 1
+b = []
 
-for i in l:
-    if i != 0:
-        print (i)
+while c != a+1:
+    if c != a+1:
+        b.append(c)
+        c = c+1
+        print (c)
     else:
-        print ('0')
-    ''''print (c)
-    print (i)
-    if i != 10000000000000000000:
-        while c != 0:
-            if c != 0:
-                b = int(input())
-                c = c + b
-                d = d + b**2
-        #print (d)
-            else:
-                print (d)
-        print (d) ''''
-    
+        print (c)
+    continue
+print (b)
+        
