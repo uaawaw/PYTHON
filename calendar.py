12509bed294f8a69698f2f97f1a9f44a96057b36
uaawaw <uaawaw@gmail.com>
2016-05-19 17:30:14 +0300

    add new file

---
+++ calendar.py
@@ -0,0 +1,7 @@
+import calendar
+calendar.prmonht(theyear, themonth, w=0, l=0) 
+''''a = calendar.LocaleHTMLCalendar(locale='Russian_Russia')
+with open('calendar.html', 'w') as g:
+    print(a.formatyear(2016, width=4), file=g)
+prmonth(theyear, themonth, w=0, l=0)''''
+
---
+++ dna_compression.py
@@ -0,0 +1,13 @@
+dna = 'sspfGGgjjjuuuuu'
+dna2 = ''
+a = dna[2]
+print (a)
+z = 1
+
+if dna[0] == dna[1]:
+    z = z+1
+    dna2 = dna[0]+z
+    print (dna2)
+else:
+    print ('Янки гоу хоум')
+
